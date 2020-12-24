9f69df857643842908e981bf450308f323ba429d
Chris Zuber <shgysk8zer0@gmail.com>
2020-12-24 12:57:35 -0800

    Update sw-config.js

--- sw-config.js
+++ sw-config.js
@@ -16,18 +16,14 @@ const config = {
 		'/img/icons.svg',
 
 		/* JS */
-		'https://cdn.polyfill.io/v3/polyfill.min.js',
-		'https://cdn.kernvalley.us/components/share-target.js',
 
 		/* `customElements` templates */
 		'https://cdn.kernvalley.us/components/toast-message.html',
-		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.html',
 		'https://cdn.kernvalley.us/components/github/user.html',
 		'https://cdn.kernvalley.us/components/pwa/prompt.html',
 
 		/* CSS */
 		'https://cdn.kernvalley.us/components/toast-message.css',
-		'https://cdn.kernvalley.us/components/share-to-button/share-to-button.css',
 		'https://cdn.kernvalley.us/components/github/user.css',
 		'https://cdn.kernvalley.us/components/pwa/prompt.css',
 
@@ -38,14 +34,6 @@ const config = {
 		'/img/icon-32.png',
 		'/img/favicon.svg',
 
-		/* Social Icons for Web Share API shim */
-		'https://cdn.kernvalley.us/img/octicons/mail.svg',
-		'https://cdn.kernvalley.us/img/logos/facebook.svg',
-		'https://cdn.kernvalley.us/img/logos/twitter.svg',
-		'https://cdn.kernvalley.us/img/logos/linkedin.svg',
-		'https://cdn.kernvalley.us/img/logos/reddit.svg',
-		'https://cdn.kernvalley.us/img/logos/gmail.svg',
-		'https://cdn.kernvalley.us/img/logos/instagram.svg',
 
 		/* Fonts */
 		'https://cdn.kernvalley.us/fonts/roboto.woff2',
@@ -54,7 +42,6 @@ const config = {
 		/* Other */
 	].map(path => new URL(path, location.origin).href),
 	allowed: [
-		/https:\/\/i\.imgur\.com\/*/,
 		/https:\/\/*\.githubusercontent\.com\/u\/*/,
 		/https:\/\/api\.github\.com\/users\/*/,
 		/https:\/\/cdn\.kernvalley\.us\/img\/*/,
