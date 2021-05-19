5cd6da2e3de72c24b8b8031ce2a6971a65e1198c
Chris Zuber <shgysk8zer0@gmail.com>
2021-05-19 06:14:06 -0700

    Misc updates & fixes

--- js/index.js
+++ js/index.js
@@ -5,16 +5,15 @@ import 'https://cdn.kernvalley.us/components/share-button.js';
 import 'https://cdn.kernvalley.us/components/share-to-button/share-to-button.js';
 import 'https://cdn.kernvalley.us/components/github/user.js';
 import 'https://cdn.kernvalley.us/components/current-year.js';
-import 'https://cdn.kernvalley.us/components/pwa/install.js';
-import { $, ready } from 'https://cdn.kernvalley.us/js/std-js/functions.js';
+import 'https://cdn.kernvalley.us/components/install/prompt.js';
+import { ready, loaded, toggleClass, on, css } from 'https://cdn.kernvalley.us/js/std-js/dom.js';
+import { getCustomElement } from 'https://cdn.kernvalley.us/js/std-js/custom-elements.js';
+import { debounce } from 'https://cdn.kernvalley.us/js/std-js/events.js';
 import { init } from 'https://cdn.kernvalley.us/js/std-js/data-handlers.js';
 import { importGa, externalHandler, telHandler, mailtoHandler } from 'https://cdn.kernvalley.us/js/std-js/google-analytics.js';
-// import { submitHandler } from './contact-demo.js';
 import { GA } from './consts.js';
 
-$(document.documentElement).css({'--viewport-height': `${window.innerHeight}px`});
-
-$(document.documentElement).toggleClass({
+toggleClass([document.documentElement], {
 	'js': true,
 	'no-js': false,
 	'no-dialog': document.createElement('dialog') instanceof HTMLUnknownElement,
@@ -22,36 +21,37 @@ $(document.documentElement).toggleClass({
 });
 
 requestIdleCallback(() => {
-	$(window).debounce('resize', () => $(':root').css({'--viewport-height': `${window.innerHeight}px`}));
-
-	$(window).on('scroll', () => {
-		requestAnimationFrame(() => {
-			$('#header').css({
-				'background-position-y': `${-0.5 * scrollY}px`,
-			});
-		});
+	on([window], {
+		resize: debounce(() => css([document.documentElerment], {'--viewport-height': `${window.innerHeight}px`})),
+		scroll: () => {
+			requestAnimationFrame(() => css('#header', { 'background-position-y': `${-0.5 * scrollY}px` }));
+		},
 	}, { passive: true });
 });
 
 if (typeof GA === 'string' && GA.length !== 0) {
-	requestIdleCallback(() => {
-		importGa(GA).then(async ({ ga }) => {
-			if (ga instanceof Function) {
-				ga('create', GA, 'auto');
-				ga('set', 'transport', 'beacon');
-				ga('send', 'pageview');
+	loaded().then(() => {
+		requestIdleCallback(() => {
+			importGa(GA).then(async ({ ga }) => {
+				if (ga instanceof Function) {
+					ga('create', GA, 'auto');
+					ga('set', 'transport', 'beacon');
+					ga('send', 'pageview');
 
-				await ready();
-
-				$('a[rel~="external"]').click(externalHandler, { passive: true, capture: true });
-				$('a[href^="tel:"]').click(telHandler, { passive: true, capture: true });
-				$('a[href^="mailto:"]').click(mailtoHandler, { passive: true, capture: true });
-			}
-
+					on('a[rel~="external"]', ['click'], externalHandler, { passive: true, capture: true });
+					on('a[href^="tel:"]', ['click'], telHandler, { passive: true, capture: true });
+					on('a[href^="mailto:"]', ['click'], mailtoHandler, { passive: true, capture: true });
+				}
+			});
 		});
 	});
 }
 
-Promise.all([
+Promise.all(
+	getCustomElement('install-prompt'),
+	ready(),
+).then(([HTMLInstallPromptElement]) => {
 	init(),
-]).catch(console.error);
+	on('#install-prompt', ['click'], () => new HTMLInstallPromptElement().show())
+		.forEach(el => el.hidden = false);
+});
